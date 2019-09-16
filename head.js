638ef8c7fdb3e41f1cf8b16729a52f78439cc216
Karen <karendavtyan21@mail.com>
2019-09-16 20:37:34 +0000

    return first element of an array

---
+++ head.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`💚💚💚Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`💔💔💔Assertion Failed: ${actual} !==  ${expected}`);
+  }
+};
+
+
+const head = function(arr){
+  return arr[0]
+}
