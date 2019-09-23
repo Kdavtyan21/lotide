b0ec1edb06d823944160636f38d07c145d289ee0
Karen <karendavtyan21@mail.com>
2019-09-23 18:26:02 +0000

    refectoring with chai

--- middle.js
+++ middle.js
@@ -1,12 +1,13 @@
 
 const middle = function (array) {
   let middleArray = []
+  mid = Math.floor(array.length / 2);
   if (array.length === 0 || array.length === 1 || array.length ===2) {
     return [];
   } else if (array.length % 2 !== 0) {
-   return [array[Math.floor(array.length/2)]];
+   return array.slice(mid, mid + 1);
   } else if (array.length % 2 === 0){
-    return [ array[Math.floor(array.length/2)-1],array[Math.floor(array.length/2)]]
+    return middleArray.push([array[Math.floor(array.length/2)-1],array[Math.floor(array.length/2)]])
   }
 };
 
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,3 +1,10 @@
-const assertEqual = require('../assertEqual')
 const tail = require('../tail')
-assertEqual(tail())
+const assert = require('chai').assert;
+
+describe("#tail", () => {
+  it("returns 2, 3 for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+});
+
+
