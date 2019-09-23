55dc7ccd4315bc5767944d47e91bba97e6683a72
Karen <karendavtyan21@mail.com>
2019-09-23 17:23:42 +0000

    refactor assertArraysEqual and add a test to it. partnered with Anthony and Nasouh

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,4 @@
-const eqArrays = function (arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-     return false;
-    }
-  }
-  return true;
-};
-
+const eqArrays = require('./eqArrays')
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
     console.log(`💚💚💚Assertion Passed: ${actual} === ${expected}`)
@@ -17,3 +6,5 @@ const assertArraysEqual = function(actual, expected) {
     console.log(`💔💔💔Assertion Failed: ${actual} !==  ${expected}`)
   }
 };
+
+module.exports = assertArraysEqual;
