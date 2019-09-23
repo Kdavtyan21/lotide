781ae7cab800b7c9f231268af6311260732fd04a
Karen <karendavtyan21@mail.com>
2019-09-23 16:25:08 +0000

    refactor tail.js paired with Anthony and Nasouh

--- tail.js
+++ tail.js
@@ -1,12 +1,8 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`💚💚💚Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`💔💔💔Assertion Failed: ${actual} !==  ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual')
 
 const tail = function(arr) {
   const newArr = arr.slice(1);
   return newArr;
 };
+
+module.exports = tail;
---
+++ test/tailTest.js
@@ -0,0 +1,3 @@
+const assertEqual = require('../assertEqual')
+const tail = require('../tail')
+assertEqual(tail())
